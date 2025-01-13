259211a17f65c470c3f2f6d087967b2ba400b7b3
Pifocode <andrey.shinkarenko.04@mail.ru>
2025-01-13 12:18:09 +0300

    Finish artefact part

--- pkg/artifact/client.go
+++ pkg/artifact/client.go
@@ -4,11 +4,45 @@ package artifact
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"net/http"
 
 	"distributed_build/pkg/build"
+	"distributed_build/pkg/tarstream"
 )
 
 // Download artifact from remote cache into local cache.
 func Download(ctx context.Context, endpoint string, c *Cache, artifactID build.ID) error {
-	panic("implement me")
+	artifactIDText := artifactID.String()
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, fmt.Sprintf("%s/artifact?id=%s", endpoint, artifactIDText), nil)
+	if err != nil {
+		return fmt.Errorf("failed to create get artifact request: %w", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("get artifact request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		errorData, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("failed to read error response: %w", err)
+		}
+		return fmt.Errorf("service error: %s", string(errorData))
+	}
+	path, commit, abort, err := c.Create(artifactID)
+	if err != nil {
+		return fmt.Errorf("unable to create artifact storage: %w", err)
+	}
+	err = tarstream.Receive(path, resp.Body)
+	if err != nil {
+		abort()
+		return fmt.Errorf("unable to save artifact: %w", err)
+	}
+	return commit()
 }
--- pkg/artifact/handler.go
+++ pkg/artifact/handler.go
@@ -3,18 +3,49 @@
 package artifact
 
 import (
+	"distributed_build/pkg/build"
+	"distributed_build/pkg/tarstream"
 	"net/http"
 
 	"go.uber.org/zap"
 )
 
 type Handler struct {
+	logger *zap.Logger
+	cache  *Cache
 }
 
 func NewHandler(l *zap.Logger, c *Cache) *Handler {
-	panic("implement me")
+	return &Handler{logger: l, cache: c}
 }
 
 func (h *Handler) Register(mux *http.ServeMux) {
-	panic("implement me")
+	mux.HandleFunc("/artifact", func(w http.ResponseWriter, r *http.Request) {
+		artifactIDData := r.URL.Query().Get("id")
+		var artifactID build.ID
+		err := artifactID.UnmarshalText([]byte(artifactIDData))
+		if err != nil {
+			errorMessage := "unable to read get by artifactID: " + err.Error()
+			h.logger.Error(errorMessage)
+			http.Error(w, errorMessage, http.StatusBadRequest)
+			return
+		}
+
+		path, unlock, err := h.cache.Get(artifactID)
+		if err != nil {
+			errorMessage := "unable to get by artifactID: " + err.Error()
+			h.logger.Error(errorMessage)
+			http.Error(w, errorMessage, http.StatusInternalServerError)
+			return
+		}
+		defer unlock()
+
+		err = tarstream.Send(path, w)
+		if err != nil {
+			errorMessage := "unable to send artifact: " + err.Error()
+			h.logger.Error(errorMessage)
+			http.Error(w, errorMessage, http.StatusInternalServerError)
+			return
+		}
+	})
 }
