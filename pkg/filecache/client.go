693de69d0654288a3beb30630b2bf5f3ceebff4b
Pifocode <andrey.shinkarenko.04@mail.ru>
2025-01-13 14:47:08 +0300

    Finish filecache part

--- pkg/filecache/client.go
+++ pkg/filecache/client.go
@@ -4,6 +4,10 @@ package filecache
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
 
 	"go.uber.org/zap"
 
@@ -11,16 +15,85 @@ import (
 )
 
 type Client struct {
+	client   *http.Client
+	logger   *zap.Logger
+	endpoint string
 }
 
 func NewClient(l *zap.Logger, endpoint string) *Client {
-	panic("implement me")
+	return &Client{
+		client:   http.DefaultClient,
+		logger:   l,
+		endpoint: endpoint,
+	}
 }
-
 func (c *Client) Upload(ctx context.Context, id build.ID, localPath string) error {
-	panic("implement me")
+	file, err := os.Open(localPath)
+	if err != nil {
+		c.logger.Error("failed to open local file", zap.String("path", localPath), zap.Error(err))
+		return err
+	}
+	defer file.Close()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPut, c.endpoint+"/file?id="+id.String(), file)
+	if err != nil {
+		c.logger.Error("failed to create request", zap.Error(err))
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/octet-stream")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		c.logger.Error("failed to perform request", zap.Error(err))
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		errorData, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("failed to read error response: %w", err)
+		}
+		return fmt.Errorf("upload failed with status: %s", string(errorData))
+	}
+
+	c.logger.Info("file uploaded successfully", zap.String("id", id.String()))
+	return nil
 }
 
 func (c *Client) Download(ctx context.Context, localCache *Cache, id build.ID) error {
-	panic("implement me")
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, c.endpoint+"/file?id="+id.String(), nil)
+	if err != nil {
+		c.logger.Error("failed to create request", zap.Error(err))
+		return err
+	}
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		c.logger.Error("failed to perform request", zap.Error(err))
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		errorData, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("failed to read error response: %w", err)
+		}
+		return fmt.Errorf("download failed with status: %s", string(errorData))
+	}
+
+	writer, abort, err := localCache.Write(id)
+	if err != nil {
+		c.logger.Error("failed to get writer for local cache", zap.Error(err))
+		return err
+	}
+	defer writer.Close()
+	if _, err := io.Copy(writer, resp.Body); err != nil {
+		c.logger.Error("failed write to local cache", zap.Error(err))
+		abort()
+		return err
+	}
+	return nil
+
 }
--- pkg/filecache/handler.go
+++ pkg/filecache/handler.go
@@ -3,18 +3,87 @@
 package filecache
 
 import (
+	"distributed_build/pkg/build"
+	"fmt"
+	"io"
 	"net/http"
+
+	"golang.org/x/sync/singleflight"
 
 	"go.uber.org/zap"
 )
 
 type Handler struct {
+	group  *singleflight.Group
+	logger *zap.Logger
+	cache  *Cache
 }
 
 func NewHandler(l *zap.Logger, cache *Cache) *Handler {
-	panic("implement me")
+	return &Handler{logger: l, cache: cache, group: new(singleflight.Group)}
 }
 
 func (h *Handler) Register(mux *http.ServeMux) {
-	panic("implement me")
+	mux.HandleFunc("/file", func(w http.ResponseWriter, r *http.Request) {
+		fileIDData := r.URL.Query().Get("id")
+		var fileID build.ID
+		err := fileID.UnmarshalText([]byte(fileIDData))
+		if err != nil {
+			errorMessage := "unable to read get by fileID: " + err.Error()
+			h.logger.Error(errorMessage)
+			http.Error(w, errorMessage, http.StatusBadRequest)
+			return
+		}
+
+		switch r.Method {
+		case http.MethodGet:
+			h.getFile(w, r, fileID)
+		case http.MethodPut:
+			h.putFile(w, r, fileID)
+		default:
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+}
+
+func (h *Handler) getFile(w http.ResponseWriter, r *http.Request, id build.ID) {
+	path, unlock, err := h.cache.Get(id)
+	if err != nil {
+		http.Error(w, "file not found", http.StatusNotFound)
+		return
+	}
+	defer unlock()
+
+	http.ServeFile(w, r, path)
+}
+
+type WriterAbort struct {
+	Writer io.WriteCloser
+	Abort  func() error
+}
+
+func (h *Handler) putFile(w http.ResponseWriter, r *http.Request, id build.ID) {
+	defer r.Body.Close()
+	_, err, _ := h.group.Do(id.String(), func() (any, error) {
+		err := h.cache.Remove(id)
+		if err != nil {
+			return nil, fmt.Errorf("unable to remove exisring file: %s", err.Error())
+		}
+		writer, abort, err := h.cache.Write(id)
+		if err != nil {
+			return nil, fmt.Errorf("invalid cache entry: %s", err.Error())
+		}
+		if _, err := io.Copy(writer, r.Body); err != nil {
+			abort() // Call abort if there's an error while writing
+			return nil, fmt.Errorf("could not write to cache: %s", err.Error())
+		}
+		defer writer.Close()
+		return nil, nil
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
