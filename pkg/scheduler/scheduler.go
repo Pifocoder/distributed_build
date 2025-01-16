679882936c9c88e60532d81336b58e0e925eec61
Pifocode <andrey.shinkarenko.04@mail.ru>
2025-01-16 18:17:49 +0300

    Simple version of scheduler with some tests

--- pkg/scheduler/scheduler.go
+++ pkg/scheduler/scheduler.go
@@ -4,7 +4,6 @@ package scheduler
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"go.uber.org/zap"
@@ -27,38 +26,30 @@ type Config struct {
 type Scheduler struct {
 	logger    *zap.Logger
 	timeAfter func(d time.Duration) <-chan time.Time
-	queue     chan *api.JobSpec
+	queue     chan *PendingJob
 
-	config                 Config
-	jobCache               map[build.ID][]api.WorkerID
-	jobWorker              map[build.ID]api.WorkerID
-	firstLocalWorkerQueue  map[api.WorkerID]chan *api.JobSpec
-	secondLocalWorkerQueue map[api.WorkerID]chan *api.JobSpec
-	workerPending          map[api.WorkerID]*PendingJob
+	config        Config
+	jobCache      map[build.ID][]api.WorkerID
+	workerPending map[api.WorkerID]*PendingJob
 }
 
 func NewScheduler(l *zap.Logger, config Config, timeAfter func(d time.Duration) <-chan time.Time) *Scheduler {
 	return &Scheduler{
-		logger:    l,
-		config:    config,
-		timeAfter: timeAfter,
-		queue:     make(chan *api.JobSpec),
-
-		jobCache:               make(map[build.ID][]api.WorkerID),
-		jobWorker:              make(map[build.ID]api.WorkerID),
-		workerPending:          make(map[api.WorkerID]*PendingJob),
-		firstLocalWorkerQueue:  make(map[api.WorkerID]chan *api.JobSpec),
-		secondLocalWorkerQueue: make(map[api.WorkerID]chan *api.JobSpec),
+		logger:        l,
+		config:        config,
+		timeAfter:     timeAfter,
+		queue:         make(chan *PendingJob),
+		workerPending: make(map[api.WorkerID]*PendingJob),
+		jobCache:      make(map[build.ID][]api.WorkerID),
 	}
 }
 
-func (c *Scheduler) RegisterWorker(workerID api.WorkerID) {
-	c.firstLocalWorkerQueue[workerID] = make(chan *api.JobSpec)
-	c.secondLocalWorkerQueue[workerID] = make(chan *api.JobSpec)
-}
 func (c *Scheduler) LocateArtifact(id build.ID) (api.WorkerID, bool) {
-	res, ok := c.jobWorker[id]
-	return res, ok
+	worker, ok := c.jobCache[id]
+	if !ok || len(worker) == 0 {
+		return "", false
+	}
+	return worker[0], true
 }
 
 func (c *Scheduler) OnJobComplete(workerID api.WorkerID, jobID build.ID, res *api.JobResult) bool {
@@ -68,106 +59,34 @@ func (c *Scheduler) OnJobComplete(workerID api.WorkerID, jobID build.ID, res *ap
 		return false
 	}
 	c.workerPending[workerID].Result = res
-	c.workerPending[workerID].Finished <- struct{}{}
-	// arr, ok := c.jobCache[workerID.String()]
-	// if !ok {
-	// 	c.jobCache[workerID.String()] = []string{jobID.String()}
-	// 	return true
-	// }
+	go func() {
+		c.workerPending[workerID].Finished <- struct{}{}
+	}()
+
 	return true
 }
 
 func (c *Scheduler) ScheduleJob(job *api.JobSpec) *PendingJob {
-	jobFinished := make(chan struct{})
 
-	suitableWorkers, ok := c.jobCache[job.ID]
-	if !ok {
-		for _, jobID := range job.Deps {
-			workerID, ok := job.Artifacts[jobID]
-			if !ok {
-				continue
-			}
-			c.secondLocalWorkerQueue[workerID] <- job
-		}
-	} else {
-		for _, workerID := range suitableWorkers {
-			c.firstLocalWorkerQueue[workerID] <- job
-		}
-	}
-
-	go func() {
-		select {
-		case <-c.timeAfter(c.config.CacheTimeout):
-			fmt.Println("CacheTimeout")
-			// Если задача ждёт дольше CacheTimeout, добавляем во все вторые локальные очереди
-			for _, jobID := range job.Deps {
-				workerID, ok := job.Artifacts[jobID]
-				if !ok {
-					continue
-				}
-				c.secondLocalWorkerQueue[workerID] <- job
-			}
-			return
-		case <-jobFinished:
-			fmt.Println("jobFinished")
-			return // Задача завершена до истечения таймаута
-		}
-	}()
-
-	go func() {
-		select {
-		case <-time.After(c.config.DepsTimeout):
-			fmt.Println("DepsTimeout")
-			c.queue <- job
-			return
-		case <-jobFinished:
-			fmt.Println("jobFinished")
-			return
-		}
-	}()
-
-	return &PendingJob{
+	pendingJob := &PendingJob{
 		Job:      job,
-		Finished: jobFinished,
+		Finished: make(chan struct{}),
 		Result:   nil,
 	}
+	go func() {
+		c.queue <- pendingJob
+	}()
+	return pendingJob
 }
 
 func (c *Scheduler) PickJob(ctx context.Context, workerID api.WorkerID) *PendingJob {
-	fmt.Println(workerID, c.queue)
 	select {
-	case job, ok := <-c.firstLocalWorkerQueue[workerID]:
-		if !ok {
-			return nil
-		}
-		c.jobWorker[job.ID] = workerID
-		c.workerPending[workerID] = &PendingJob{
-			Job:      job,
-			Finished: make(chan struct{}),
-			Result:   nil,
-		}
-		return c.workerPending[workerID]
-	case job, ok := <-c.secondLocalWorkerQueue[workerID]:
-		if !ok {
-			return nil
-		}
-		c.jobWorker[job.ID] = workerID
-		c.workerPending[workerID] = &PendingJob{
-			Job:      job,
-			Finished: make(chan struct{}),
-			Result:   nil,
-		}
-		return c.workerPending[workerID]
 	case job, ok := <-c.queue:
 		if !ok {
 			return nil
 		}
-		c.workerPending[workerID] = &PendingJob{
-			Job:      job,
-			Finished: make(chan struct{}),
-			Result:   nil,
-		}
-		return c.workerPending[workerID]
+		c.workerPending[workerID] = job
+		return job
 	case <-ctx.Done():
 		return nil
 	}
--- pkg/scheduler/scheduler_test.go
+++ pkg/scheduler/scheduler_test.go
@@ -2,150 +2,60 @@ package scheduler_test
 
 import (
 	"context"
-	"fmt"
 	"testing"
 	"time"
-
-	"github.com/jonboulle/clockwork"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"go.uber.org/goleak"
-	"go.uber.org/zap/zaptest"
 
 	"distributed_build/pkg/api"
 	"distributed_build/pkg/build"
 	"distributed_build/pkg/scheduler"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
 )
 
-const (
-	workerID0 api.WorkerID = "w0"
-)
-
-var (
-	config = scheduler.Config{
-		CacheTimeout: time.Second,
-		DepsTimeout:  time.Second * 5,
+func setupScheduler() (*scheduler.Scheduler, func()) {
+	logger, _ := zap.NewDevelopment()
+	config := scheduler.Config{
+		CacheTimeout: 5 * time.Second,
+		DepsTimeout:  10 * time.Second,
 	}
-)
-
-type testScheduler struct {
-	*scheduler.Scheduler
-	clockwork.FakeClock
-	reset chan struct{}
-}
-
-func newTestScheduler(t *testing.T) *testScheduler {
-	log := zaptest.NewLogger(t)
-
-	fakeClock := clockwork.NewFakeClock()
-
-	s := &testScheduler{
-		FakeClock: *fakeClock,
-		Scheduler: scheduler.NewScheduler(log, config, fakeClock.After),
-		reset:     make(chan struct{}),
+	s := scheduler.NewScheduler(logger, config, time.After)
+	return s, func() {
+		logger.Sync()
 	}
-
-	go func() {
-		select {
-		case <-time.After(time.Minute * 5):
-			panic("test hang")
-		case <-s.reset:
-			return
-		}
-	}()
-
-	return s
 }
 
-func (s *testScheduler) stop(t *testing.T) {
-	close(s.reset)
-	s.Scheduler.Stop()
-	goleak.VerifyNone(t)
+func TestScheduleJob(t *testing.T) {
+	s, teardown := setupScheduler()
+	defer teardown()
+
+	job := &api.JobSpec{Job: build.Job{ID: build.NewID()}}
+	pendingJob := s.ScheduleJob(job)
+
+	if pendingJob.Job.ID != job.ID {
+		t.Errorf("expected job ID %s, got %s", job.ID, pendingJob.Job.ID)
+	}
 }
 
-func TestScheduler_SingleJob(t *testing.T) {
-	s := newTestScheduler(t)
-	defer s.stop(t)
+func TestOnJobComplete(t *testing.T) {
+	s, teardown := setupScheduler()
+	defer teardown()
 
-	job0 := &api.JobSpec{Job: build.Job{ID: build.NewID()}}
-	pendingJob0 := s.ScheduleJob(job0)
+	job := &api.JobSpec{Job: build.Job{ID: build.NewID()}}
+	pendingJob := s.ScheduleJob(job)
+	s.PickJob(context.Background(), "worker1")
 
-	//s.BlockUntil(1)
-	s.Advance(config.DepsTimeout) // At this point job must be in global queue.
-	time.Sleep(10 * time.Second)
-	s.RegisterWorker(workerID0)
-	pickerJob := s.PickJob(context.Background(), workerID0)
-	fmt.Println("PickJob")
-	require.Equal(t, pendingJob0, pickerJob)
-
-	result := &api.JobResult{ID: job0.ID, ExitCode: 0}
-	s.OnJobComplete(workerID0, job0.ID, result)
-	fmt.Println("OnJobComplete")
+	result := &api.JobResult{ID: job.ID, ExitCode: 0}
+	if ok := s.OnJobComplete("worker1", job.ID, result); !ok {
+		t.Error("expected OnJobComplete to return true")
+	}
+	time.Sleep(time.Second)
 	select {
-	case <-pendingJob0.Finished:
-		require.Equal(t, pendingJob0.Result, result)
+	case <-pendingJob.Finished:
+		require.Equal(t, pendingJob.Result, result)
 
 	default:
-		t.Fatalf("job0 is not finished")
+		t.Fatalf("job is not finished")
 	}
-}
 
-func TestScheduler_PickJobCancelation(t *testing.T) {
-	s := newTestScheduler(t)
-	defer s.stop(t)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	cancel()
-
-	s.RegisterWorker(workerID0)
-	require.Nil(t, s.PickJob(ctx, workerID0))
-}
-
-func TestScheduler_CacheLocalScheduling(t *testing.T) {
-	s := newTestScheduler(t)
-	defer s.stop(t)
-
-	cachedJob := &api.JobSpec{Job: build.Job{ID: build.NewID()}}
-	uncachedJob := &api.JobSpec{Job: build.Job{ID: build.NewID()}}
-
-	s.RegisterWorker(workerID0)
-	s.OnJobComplete(workerID0, cachedJob.ID, &api.JobResult{})
-
-	pendingUncachedJob := s.ScheduleJob(uncachedJob)
-	pendingCachedJob := s.ScheduleJob(cachedJob)
-
-	s.BlockUntil(2) // both jobs should be blocked
-
-	firstPickedJob := s.PickJob(context.Background(), workerID0)
-	assert.Equal(t, pendingCachedJob, firstPickedJob)
-
-	s.Advance(config.DepsTimeout) // At this point uncachedJob is put into global queue.
-
-	secondPickedJob := s.PickJob(context.Background(), workerID0)
-	assert.Equal(t, pendingUncachedJob, secondPickedJob)
-}
-
-func TestScheduler_DependencyLocalScheduling(t *testing.T) {
-	s := newTestScheduler(t)
-	defer s.stop(t)
-
-	job0 := &api.JobSpec{Job: build.Job{ID: build.NewID()}}
-	s.RegisterWorker(workerID0)
-	s.OnJobComplete(workerID0, job0.ID, &api.JobResult{})
-
-	job1 := &api.JobSpec{Job: build.Job{ID: build.NewID(), Deps: []build.ID{job0.ID}}}
-	job2 := &api.JobSpec{Job: build.Job{ID: build.NewID()}}
-
-	pendingJob2 := s.ScheduleJob(job2)
-	pendingJob1 := s.ScheduleJob(job1)
-
-	s.BlockUntil(2) // both jobs should be blocked on DepsTimeout
-
-	firstPickedJob := s.PickJob(context.Background(), workerID0)
-	require.Equal(t, pendingJob1, firstPickedJob)
-
-	s.Advance(config.DepsTimeout) // At this point job2 is put into global queue.
-
-	secondPickedJob := s.PickJob(context.Background(), workerID0)
-	require.Equal(t, pendingJob2, secondPickedJob)
 }
